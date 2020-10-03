3bb4d089f7293130ce96a25e9162b9b6a303aafd
ruchiagrawal <33382405+ruchi004@users.noreply.github.com>
2020-10-03 18:22:53 +0000

    Longest common subsequence using dynamic programming

---
+++ Longest_common_subsequence.py
@@ -0,0 +1,14 @@
+def lps(x,y):
+	m , n = len(x) , len(y)
+	mat = [[0 for i in range(m+1)] for i in range(n+1)]
+	for indx , i in enumerate(y):
+		for indy , j in enumerate(x):
+			if i==j:
+				mat[indx+1][indy+1] = mat[indx][indy] + 1
+			else:
+				mat[indx+1][indy+1] = max(mat[indx+1][indy] , mat[indx][indy+1])
+
+	print(mat[n][m])
+
+x = str(input())
+lps(x,x[::-1])
